e8505e6f27dac0469d73c6772a2a09c9dbb34b0b
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-03-12 21:05:22 -0400

    store data in figmaplugin data

--- code.ts
+++ code.ts
@@ -2,6 +2,18 @@ figma.showUI(__html__);
 figma.ui.onmessage =  async (msg: {type: string, count: number, value: any}) => {
   if (msg.type === 'result') {
     const nodes: SceneNode[] = [];
+    let pulsedata = JSON.parse(figma.root.getPluginData("pulsedata") || "[]");
+
+
+    let newReaction = {
+      x: Math.random() * 500, 
+      y: Math.random() * 500, 
+      emoji: msg.value
+    };
+
+    pulsedata.push(newReaction);
+    figma.root.setPluginData("pulsedata", JSON.stringify(pulsedata));
+    console.log(pulsedata)
     const note = figma.createSticky();
     await figma.loadFontAsync(note.text.fontName as FontName)
     note.text.characters = msg.value;
