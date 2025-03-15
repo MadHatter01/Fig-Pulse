b1aa2edcf2648fe49fb59bbfe5ed878d38796e81
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-03-15 18:40:04 -0400

    load existing reactions

--- code.ts
+++ code.ts
@@ -1,19 +1,37 @@
 figma.showUI(__html__);
+
+type PulseReaction = {x: number, y: number, emoji: string};
+
+let pulseData: PulseReaction[] = JSON.parse(figma.root.getPluginData("pulsedata") || "[]");
+
+const loadExisting = async ()=>{
+  const nodes: SceneNode[] = [];
+
+  for (const reaction of pulseData){
+    const note = figma.createSticky();
+    note.x = reaction.x;
+    note.y = reaction.y;
+    await figma.loadFontAsync(note.text.fontName as FontName)
+    note.text.characters = reaction.emoji;
+    figma.currentPage.appendChild(note);
+    nodes.push(note)
+  }
+  figma.viewport.scrollAndZoomIntoView(nodes);
+}
+
 figma.ui.onmessage =  async (msg: {type: string, count: number, value: any}) => {
   if (msg.type === 'result') {
     const nodes: SceneNode[] = [];
-    let pulsedata = JSON.parse(figma.root.getPluginData("pulsedata") || "[]");
 
-
-    let newReaction = {
+    let newReaction: PulseReaction = {
       x: Math.random() * 500, 
       y: Math.random() * 500, 
       emoji: msg.value
     };
 
-    pulsedata.push(newReaction);
-    figma.root.setPluginData("pulsedata", JSON.stringify(pulsedata));
-    console.log(pulsedata)
+    pulseData.push(newReaction);
+    figma.root.setPluginData("pulsedata", JSON.stringify(pulseData));
+
     const note = figma.createSticky();
     await figma.loadFontAsync(note.text.fontName as FontName)
     note.text.characters = msg.value;
@@ -24,3 +42,5 @@ figma.ui.onmessage =  async (msg: {type: string, count: number, value: any}) => 
   }
   // figma.closePlugin();
 };
+
+loadExisting();
